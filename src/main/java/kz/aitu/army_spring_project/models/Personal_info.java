8273bfef93a6727d06d6bab425667215e992d3b1
aigera <153474527+aigerim1q@users.noreply.github.com>
2024-02-25 21:41:32 +0600

    Personal_info

--- src/main/java/kz/aitu/army_spring_project/models/Personal_info.java
+++ src/main/java/kz/aitu/army_spring_project/models/Personal_info.java
@@ -1,4 +1,22 @@
-package kz.aitu.army_spring_project.models;
+package kz.aitu.army_spring.models;
 
-public class Personal_info {
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "personal_info")
+public class Personal_info extends ID{
+    private String name;
+    private String surname;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Candidate №" +getId()+
+                " Name: " + name +
+                " Surname: " + surname  +
+                " Age: " + age;
+    }
+
 }
